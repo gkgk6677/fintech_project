2ffd4eb22321d0c1b8e14583ae1169581846a003
seo <gkgk6677@naver.com>
2019-09-17 10:43:09 +0900

    add function.js

---
+++ day01/function.js
@@ -0,0 +1,16 @@
+function divideFunction(p1,p2){
+    return p1/p2;
+};
+function addFunction(p1,p2){
+    return p1+p2;
+}
+function minusFunction(p1,p2){
+    return p1-p2;
+}
+function multiFunction(p1,p2){
+    return p1*p2;
+}
+console.log(addFunction(1,2))
+console.log(minusFunction(10,5))
+console.log(multiFunction(3,4))
+console.log(divideFunction(10,5))
