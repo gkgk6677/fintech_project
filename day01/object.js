a96df3e9188a8b035fca6992775c3fc18472d068
seo <gkgk6677@naver.com>
2019-09-17 10:51:16 +0900

    add object.js

---
+++ day01/object.js
@@ -0,0 +1,11 @@
+var car = {
+    name : "sonata",
+    ph : "500ph",
+    start : function(){
+        console.log("engine is starting");
+    },
+    stop : function(){
+        console.log("engine is stopped");
+    }
+}
+console.log(car);
