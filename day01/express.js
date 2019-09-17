4e56eb08e5c52d008468dc7c3e3c34aea3be6327
seo <gkgk6677@naver.com>
2019-09-17 15:11:16 +0900

    view 및 ejs 추가

--- day01/express.js
+++ day01/express.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 app = express();
 
+//view 및 ejs 추가
+app.set('views', __dirname+'/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 var port = process.env.port || 8080;
 
 // public 폴더 내에 있는 파일들(css 같은 것)을 적용할 때 위치를 routing해서 지정해주는 역할
